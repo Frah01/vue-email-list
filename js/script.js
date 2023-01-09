e063cc65125e8aba56dd16343f1f10ae2c848be0
Frah01 <vinario.francesco@gmail.com>
2023-01-09 13:44:40 +0100

    primo push

--- js/script.js
+++ js/script.js
@@ -3,7 +3,17 @@ const {createApp} = Vue
 createApp({
     data(){
         return{
-            
+            arrayNumber: []
         }
-    }
+    },
+    methods: {
+        getEmails(){
+            for(let i=0; i>10 ; i++){
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) =>{
+                    this.arrayNumber.push(result.data.response)
+                })
+                console.log(arrayNumber)
+            }
+        }
+    },
 }).mount('#app')
