e3a759aa63c555f1eba63d88b4b72fd97aeecbe7
Frah01 <vinario.francesco@gmail.com>
2023-01-09 16:10:30 +0100

    Esercizio completo con bonus

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
-const {createApp} = Vue
+const {
+    createApp
+} = Vue;
 
 createApp({
     data(){
         return{
-            
+            emailArray: [],
         }
-    }
+    },
+    mounted() {
+        for(let i = 0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result)=> {
+                this.email = result.data.response;
+                this.emailArray.push(this.email);
+            });
+        }
+
+    },
 }).mount('#app')
