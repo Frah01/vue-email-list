8787a7e8aeeeeaeb54292d8e6de76ca1e270fc58
Frah01 <vinario.francesco@gmail.com>
2023-01-10 09:44:46 +0100

    Aggiunto console.log

--- js/script.js
+++ js/script.js
@@ -13,8 +13,8 @@ createApp({
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result)=> {
                 this.email = result.data.response;
                 this.emailArray.push(this.email);
+                console.log(result)
             });
         }
-
     },
 }).mount('#app')
